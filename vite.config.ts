8b9d360c7fe7ba0226203116d035ed5508343549
ferdyasino <ferdyasino@gmail.com>
2026-07-18 12:45:08 +0800

    versel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+  plugins: [react(), tsconfigPaths()],
+
   server: {
     proxy: {
       "/gas": {
